c32dec066074597e6a275d492ffe0f9f8f03b796
natebowang <120029790@qq.com>
2021-02-04 15:41:22 +1300

    Update app.js. Delete unnecessary middlewares.

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET user listing. */
 router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
