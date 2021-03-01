763013dbfce26c440a142c8019c83d7249f48b5b
natebowang <120029790@qq.com>
2021-03-01 19:07:35 +1300

    Add session login.

--- routes/login.js
+++ routes/login.js
@@ -8,6 +8,32 @@ const router = express.Router();
 
 // curl -v -c ~/cookie -d '{ "username":"gy", "password": "gy"}' -H "Content-Type:application/json" http://localhost:8000/api/login
 router.post('/', async (req, res, next) => {
+    if (req.body.username !== "") {
+        await usernameLoginMiddleWare(req, res, next)
+    } else if (req.session.username) {
+        await sessionLoginMiddleWare(req, res, next)
+    } else {
+        let errorMessage = 'Incorrect username or password';
+        let errorProps = {incorrectUsernamePwd: true};
+        next(createError(401, errorMessage, {errorProps: errorProps}));
+    }
+});
+
+const sessionLoginMiddleWare = async (req, res, next) => {
+    let username = req.session.username;
+    let sqlResult = await pgPool.query(
+        'SELECT email, phone, avatar FROM users where username=$1',
+        [username]);
+    const responseUserObject = {
+        username: username,
+        email: sqlResult.rows[0].email,
+        phone: sqlResult.rows[0].phone,
+        avatar: sqlResult.rows[0].avatar,
+    };
+    await res.json(responseUserObject);
+};
+
+const usernameLoginMiddleWare = async (req, res, next) => {
     let errorMessage = '';
     let errorProps = {};
 
@@ -25,8 +51,10 @@ router.post('/', async (req, res, next) => {
         const salt = result.rows[0].salt;
         const passwordSha = sha.update(password + salt).digest('hex');
         let row = result.rows[0];
+        // Correct password
         if (row.user_state === 0 && row.login_method_id === 0 && passwordSha === row.password) {
             req.session.username = username;
+            console.log(req.session.id)
             const responseUserObject = {
                 username: username,
                 email: row.email,
@@ -34,11 +62,11 @@ router.post('/', async (req, res, next) => {
                 avatar: row.avatar,
             };
             await res.json(responseUserObject);
-        } else if (row.login_method_id !== 0) {
+        } else if (row.login_method_id !== 0) { // Should use other login method
             errorMessage = 'You should continue with another login method.';
             errorProps = Object.assign(errorProps, {loginMethodId: row.login_method_id});
             next(createError(401, errorMessage, {errorProps: errorProps}));
-        } else if (row.user_state !== 0) {
+        } else if (row.user_state !== 0) { // User is disabled
             errorMessage = 'User is disabled.';
             errorProps = Object.assign(errorProps, {userDisabled: true});
             next(createError(401, errorMessage, {errorProps: errorProps}));
@@ -52,6 +80,7 @@ router.post('/', async (req, res, next) => {
         errorProps = Object.assign(errorProps, {incorrectUsernamePwd: true});
         next(createError(401, errorMessage, {errorProps: errorProps}));
     }
-});
+
+};
 
 module.exports = router;
